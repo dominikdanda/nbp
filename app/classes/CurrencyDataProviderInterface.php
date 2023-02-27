22241eb75b18bca2671d0c9b58a6249a5f62b57f
Dominik Dańda <alchemiko@wp.pl>
2023-02-27 23:39:10 +0100

    fix: use constant for API URI

--- app/classes/CurrencyDataProviderInterface.php
+++ app/classes/CurrencyDataProviderInterface.php
@@ -2,7 +2,5 @@
 
 interface CurrencyDataProviderInterface {
 
-	public function setNbpApiUri(string $uri): void;
-
 	public function getExchangeRates(string $currencyCode, string $startDate, string $endDate): array;
 }
